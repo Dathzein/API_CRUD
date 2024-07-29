6b5f328015f8d2d31f0259e3328bb931d02df845
Yohan Guaraca <yohan.guaraca96@gmail.com>
2024-07-28 23:02:22 -0500

    Actualizacion de validacion de clases. ultimo commit.

--- Models/Dtos/Customer.cs
+++ Models/Dtos/Customer.cs
@@ -14,12 +14,20 @@ namespace Models.Dtos
         [Required(ErrorMessage = "Nombre es requerido")]
         //[StringLength(255,MinimumLength =1, ErrorMessage ="No puede estar vacio")]
         public string? nombre { get; set; }
+        [Required(ErrorMessage = "tipo de identificacion es requerido")]
         public int codigo_identificacion { get; set; }
+        [Required(ErrorMessage = "Numero de identificacion es requerido")]
         public string? numero_identificacion { get; set; }
+        [Required(ErrorMessage = "Correo es requerido")]
+        [EmailAddress]
         public string? correo_electronico { get; set; }
+        [Required(ErrorMessage = "Edad es requerida")]
         public int edad { get; set; }
+        [Required(ErrorMessage = "Codigo de pais es requerido")]
         public int codigo_pais { get; set; }
+        [Required(ErrorMessage = "Numero de telefono es requerido")]
         public string? numero_telefono { get; set; }
+        [Required(ErrorMessage = "Estado es requerido")]
         public bool active { get; set; }
    
     }
